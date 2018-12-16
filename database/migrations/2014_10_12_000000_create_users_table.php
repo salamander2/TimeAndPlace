3721a578a577bbab201f5f67e77d8b907ab976dd
Salamander2 <mk_gecko@yahoo.com>
2018-12-16 16:10:55 -0500

    working with migrations and models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,14 +14,14 @@ class CreateUsersTable extends Migration
 	public function up()
 	{
 		Schema::create('users', function (Blueprint $table) {
-				$table->increments('id');
-				$table->string('username');
+				//$table->increments('id');
+				$table->string('username')->unique();
 				$table->string('fullname');
-				$table->boolean('isAdmin');
-				$table->boolean('viewAll');
+				$table->boolean('isAdmin')->default(0);
+				$table->boolean('viewAll')->default(0);
 				//$table->string('email')->unique();
 				$table->string('password');
-				$table->boolean('defaultPWD');
+				$table->boolean('defaultPWD')->default(1);
 				//$table->rememberToken();
 				$table->timestamps();
 				});
