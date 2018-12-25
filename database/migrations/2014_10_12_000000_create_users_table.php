18e303dac172e0ffdcf2d7dc5263eb491a4111f4
Salamander2 <mk_gecko@yahoo.com>
2018-12-25 08:50:00 -0500

    Decided to add back in the remember_token. I don't have to use it if I don't want to. Also had to revert ./vendor/laravel/framework/src/Illuminate/Auth/SessionGuard.php to original.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
 				//$table->string('email')->unique();
 				$table->string('password');
 				$table->boolean('defaultPWD')->default(1);
-				//$table->rememberToken();
+				$table->rememberToken();
 				$table->timestamps();
 				});
 	}
