c6c74e2c2f3308e189711f45c50d3c275fad05b9
Salamander2 <mk_gecko@yahoo.com>
2019-01-18 16:26:46 -0500

    Adding kiosk to routes, testing vimdiff mergetooly

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. 
+|
 |  References: https://laracasts.com/discuss/channels/laravel/routehas
 */
 
@@ -30,7 +31,6 @@
 
 /*-------------Auth routes  (Authentication Routes...) -----------------*/
 Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
-//Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
@@ -45,7 +45,7 @@ Route::post('/changePassword','UserController@changePassword')->name('changePass
 /*----------------Admin Routes------------------*/
 /*........ kiosk handling routes ................*/
 Route::get('/addKiosk', 'AdminController@addKiosk');
-//ADD ALL ROUTES HERE!
+Route::delete('','AdminController@deleteKiosk');
 
 
 /*........ user handling routes ................*/
@@ -60,6 +60,10 @@ Route::get('/userMaint', function() {
 	return view('userMaint');
 });
 */
+
+/*----------------Kiosk Routes-------------*/
+// add, delete are in Admin
+
 
 /*----------------Attendance Routes-------------*/
 
