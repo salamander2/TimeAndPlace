41576323747a0304525d35189baaa8256fd1ca64
Salamander2 <mk_gecko@yahoo.com>
2019-03-18 21:58:09 -0400

    Added daterangepicker. About to test JS and jquery script imports

--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
 <script type="text/javascript">
     $(document).ready(function(){
         alert("hi");
