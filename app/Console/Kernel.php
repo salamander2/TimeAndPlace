324dbb123e5a356e3a3e265bea15d29e4e654e78
Salamander2 <mk_gecko@yahoo.com>
2019-04-18 21:48:15 -0400

    Modified autosignout a bit. Also put the red autosignout button into Adminstrator options

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,37 +9,37 @@ use Illuminate\Support\Facades\DB;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        'App\Console\Commands\SignoutStudents',
-        'App\Console\Commands\ScheduleList',
-    ];
+	/**
+	 * The Artisan commands provided by your application.
+	 *
+	 * @var array
+	 */
+	protected $commands = [
+		'App\Console\Commands\SignoutStudents',
+		'App\Console\Commands\ScheduleList',
+	];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-	 try {
-            $kioskSched = [];
-            if (Schema::hasTable('kiosk_schedule')) {
-                $kioskSched = DB::table('kiosk_schedule')->get();
-            }
-            if (Schema::hasTable('schedules')) {
-                $scheds = DB::table('schedules')->get();
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule)
+	{
+		try {
+			$kioskSched = [];
+			if (Schema::hasTable('kiosk_schedule')) {
+				$kioskSched = DB::table('kiosk_schedule')->get();
+			}
+			if (Schema::hasTable('schedules')) {
+				$scheds = DB::table('schedules')->get();
 			}
 
 			//GET will return the 292nd item. For some reason it is not returning the item with id=292.
 			//	dd($scheds->get(292));
 
-            foreach ($kioskSched as $entry) {
+			foreach ($kioskSched as $entry) {
 				$schedID = $entry->schedule_id;
 				//echo $schedID.PHP_EOL;
 				$time = $scheds->where('id',$schedID)->first()->end;
@@ -49,8 +49,8 @@ class Kernel extends ConsoleKernel
 				//	$time = $scheds->find($schedID)->altend;
 
 				//Also pass in the time so that the command can check if they have signed in 5 minutes before, and thus ignore them.
-                $schedule->command('kiosk:signoutstudents '.$entry->kiosk_id.' '.$time)->at($time);
-            }
+				$schedule->command('kiosk:signoutstudents '.$entry->kiosk_id.' '.$time)->at($time);
+			}
 
 			//Debugging why this doesn't work. Can't use dailyAt(), use at() instead.
 			//$cmd = 'kiosk:signoutstudents 1 21:40';
@@ -60,21 +60,21 @@ class Kernel extends ConsoleKernel
 			//--massive dump!			var_dump($schedule);
 			//			echo $schedule;
 
-        } catch (\Exception $e) {
+		} catch (\Exception $e) {
 			//TODO DIE!!! -- record error somewhere
 			echo "SQL table open error in app/Console/Kernel.php";
 		}
-    }
+	}
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+	/**
+	 * Register the commands for the application.
+	 *
+	 * @return void
+	 */
+	protected function commands()
+	{
+		$this->load(__DIR__.'/Commands');
 
-        require base_path('routes/console.php');
-    }
+		require base_path('routes/console.php');
+	}
 }
