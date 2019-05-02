ec4b34f567e1442e179fc5eeed2829c4445ba80d
Salamander2 <mk_gecko@yahoo.com>
2019-05-02 09:28:59 -0400

    In process of fixing up help screen

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -22,6 +22,88 @@
 
 @section('content')
 
+ <div class="row">
+          <div class="col-12">
+            <!-- Custom Tabs -->
+            <div class="card">
+              <div class="card-header d-flex p-0">
+                <ul class="nav nav-pills p-2">
+                  <li class="nav-item"><a class="nav-link active" href="#tab_1" data-toggle="tab">Purpose</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#tab_2" data-toggle="tab">Roles</a></li>
+                  <li class="nav-item"><a class="nav-link" href="#tab_3" data-toggle="tab">Kiosk Settings</a></li>
+                </ul>
+              </div><!-- /.card-header -->
+              <div class="card-body">
+                <div class="tab-content">
+                  <div class="tab-pane active" id="tab_1">
+                    A wonderful serenity has taken possession of my entire soul,
+                    like these sweet mornings of spring which I enjoy with my whole heart.
+                    I am alone, and feel the charm of existence in this spot,
+                    which was created for the bliss of souls like mine. I am so happy,
+                    my dear friend, so absorbed in the exquisite sense of mere tranquil existence,
+                    that I neglect my talents. I should be incapable of drawing a single stroke
+                    at the present moment; and yet I feel that I never was a greater artist than now.
+                  </div>
+                  <!-- /.tab-pane -->
+                  <div class="tab-pane" id="tab_2">
+		<div class="card card-warning">
+		    <div class="card-header"><h4>Database administrators</h4></div>
+		    <div class="card-body">
+			<p><b>The two administrators are Michael Harwood and Larry Farquahrson.</b></p>
+			<p>The administrators are the only ones who can:<br>
+			<ol><li>add users and delete users
+			<li>add a new kiosk, set the initial kiosk admin, and delete kiosks
+			</ol>
+		    </div>
+		</div>
+
+		<div class="card card-warning">
+		    <div class="card-header"><h4>Kiosk administrators</h4></div>
+		    <div class="card-body">
+			<p>Each kiosk has one or more kiosk admins.<br> The kiosk admins add users to the kiosk. 
+			Kiosk admins can also change most of the settings of the kiosk (e.g. making the logs private/public).</p>
+		    </div>
+		</div>
+
+		<div class="card card-dark">
+		    <div class="card-header"><h4>Kiosk users</h4></div>
+		    <div class="card-body">
+			<p>Users can start the start the Terminals of their own kiosks. 
+			Only kiosk users (and kiosk admins) can view the logs of a kiosk if the kiosk is set to private.
+			</p>
+		    </div>
+		</div>
+
+		<div class="card card-dark">
+		    <div class="card-header"><h4>General users</h4></div>
+		    <div class="card-body">
+			<p>Any user can view the settings of any kiosk and see who the users/admins of that kiosk are<br>
+			Any user can view public logs.</p>
+			<p>There is a generic teacher login which acts like a user who is not assigned to any kiosks.</p>
+		    </div>
+		</div>
+                </div>
+                  <!-- /.tab-pane -->
+                  <div class="tab-pane" id="tab_3">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    It has survived not only five centuries, but also the leap into electronic typesetting,
+                    remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                    sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                    like Aldus PageMaker including versions of Lorem Ipsum.
+                  </div>
+                  <!-- /.tab-pane -->
+                </div>
+                <!-- /.tab-content -->
+              </div><!-- /.card-body -->
+            </div>
+            <!-- ./card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+        <!-- END CUSTOM TABS -->
 
 <div class="container-fluid">
 <div class="row">
