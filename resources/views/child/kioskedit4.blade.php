7077f32b9f914fc00dc56988cb625efb3b6414c4
Salamander2 <mk_gecko@yahoo.com>
2019-12-05 21:02:48 -0500

    working on allowing modifications to event times.

--- resources/views/child/kioskedit4.blade.php
+++ resources/views/child/kioskedit4.blade.php
@@ -17,37 +17,49 @@ $(function () {
         
         <div class="row my-1" style="border-bottom:solid black 1px;">
             <div class="col-md-3">#&nbsp;&nbsp;Name</div>
-            <div class="col">&nbsp;</div>
             <div class="col">Date</div>
             <div class="col">Start Time</div>
             <div class="col">Late Time</div>
             <div class="col">End Time</div>
+            <div class="col">&nbsp;</div>
         </div>
-        @foreach($kiosk->events as $event)
-            <div class="row py-1">
+        @foreach($kiosk->events->sortBy('date') as $event)
+            <div class="row py-1 border border-left-0 border-right-0 border-top-0">
                 <div class="col-md-3">
                         {{--  {{$event->id}} <a href="/events/{{$event->id}}/addStudents"><button type="button" class=" btn btn-primary" data-toggle="tooltip" data-placement="top" title="Click to add students to this event">  --}}
-                        {{$event->id}}&nbsp;&nbsp;<span class="btn bg-info">{{ $event->name }}</span>
+                        @if($event->date < now())
+                        {{$event->id}}&nbsp;&nbsp;<a href="{{'/events/settings/'.$event->id}}"><button type="button" class="elevation-3 btn bg-info">{{ $event->name }}</button></a>
+                        @else
+                        {{$event->id}}&nbsp;&nbsp;<a href="{{'/events/settings/'.$event->id}}"><button type="button" class="elevation-3 btn btn-outline-warning bg-info" style="border-width:3px;">{{ $event->name }}</button></a>
+                        @endif
+                        {{-- {{$event->id}}&nbsp;&nbsp;<span class="btn bg-info">{{ $event->name }}</span> --}}
+                        {{-- {{$event->id}}&nbsp;&nbsp;<span class="btn btn-outline-warning bg-info">{{ $event->name }}</span> --}}
                     {{--  <input type="text" class="form-control bg-primary" disabled  value="{{ $event->name }}">  --}}
                 </div>
                 <div class="col">
-                    <a href="{{'/events/settings/'.$event->id}}"><button type="button" class="btn btn-outline-danger">Options</button></a>
-                </div>
-                <div class="col">
-                    <label>{{$event->date}} </label>
+            <form>
+                    <input type="date" class="border border-secondary" id="date" name="date" value="{{ $event->date }}" required>
+                    {{-- <label>{{$event->date}} </label> --}}
                     {{--  <input type="date" class="form-control border border-info" disabled id="date" name="date" value="{{ $event->date }}">  --}}
                 </div>
                 <div class="col">
                         {{--  <label>{{$event->startTime}} </label>  --}}
-                        <input style="background-color:white;color:black;border:0" type="time"  readonly id="startTime" name="startTime" value="{{ $event->startTime }}"> 
+                        <input type="time" class="border border-secondary" id="startTime" name="startTime" value="{{ $event->startTime }}" required>
                 </div>
                 <div class="col">
                         {{--  <label>{{$event->lateTime}} </label>  --}}
-                        <input style="background-color:white;color:black;border:0" type="time" readonly id="lateTime" name="lateTime" value="{{ $event->lateTime }}">
+                        <input type="time" class="border border-secondary" id="startTime" name="startTime" value="{{ $event->lateTime }}" required>
+                        {{-- <input style="background-color:white;color:black;border:0" type="time" readonly id="lateTime" name="lateTime" value="{{ $event->lateTime }}"> --}}
                 </div>
                 <div class="col">
                         {{--  <label>{{$event->endTime}} </label>  --}}
-                    <input style="background-color:white;color:black;border:0" type="time" readonly id="endTime" name="endTime" value="{{ $event->endTime }}">
+                        <input type="time" class="border border-secondary" id="startTime" name="startTime" value="{{ $event->endTime }}" required>
+                </div>
+                <div class="col">
+                    {{-- <a href="{{'/events/settings/'.$event->id}}"><button type="button" class="btn btn-outline-danger">Options</button></a> --}}
+                    <a href=""><button type="button" class="btn btn-outline-success elevation-2">Update</button></a>&nbsp;&nbsp;
+            </form>
+                    <a href=""><button type="button" class="btn btn-outline-danger elevation-2"><i class="fas fa-skull-crossbones"></i>&nbsp;</button></a>
                 </div>
             </div>
             <!-- end row -->
