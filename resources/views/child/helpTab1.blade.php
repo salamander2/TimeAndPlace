f463b1317c394787839cb3a7cf5af8fd9cfd983d
Michael Harwood <mk_gecko@yahoo.com>
2020-10-23 12:24:56 +0000

    Updating Help screens

---
+++ resources/views/child/helpTab1.blade.php
@@ -0,0 +1,92 @@
+<div class="card card-warning">
+<div class="card-header">
+<h4>Terminology</h4>
+<div class="card-tools">
+<button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fa fa-minus"></i></button>
+</div>
+</div>
+<div class="card-body">
+<h5><b><u>Kiosk:</u></b> the team, club, group, department, ... that is tracking attendance.
+Because this is program flexible enough to encompass all of these different
+groups/teams/classes/categories, "kiosk" is being used as the general term for them all.
+</h5>
+<h5><b><u>Terminal:</u></b> the screen that is used for students to log in and log out. </h5>
+<h5><i class="icon fa fa-star"></i> The Home screen shows the students who are currently signed in to
+all the public kiosks</h5>
+</div>
+</div><!-- end card -->
+
+<div class="card card-success">
+<div class="card-header">
+<h4>Purpose</h4>
+<div class="card-tools">
+<button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fa fa-minus"></i></button>
+</div>
+</div>
+
+<div class="card-body">
+<h5>This program is designed to enable a variety of groups such as classes, teams, clubs, departments,
+	to track students' attendance/location.</h5> 
+	<div class="alert alert-info col-md-6">
+	<h5><i class="icon fa fa-info"></i> There are three possible modes</h5>
+	</div>
+
+<div class="callout callout-info">
+<h5 class="text-dark">1. <u>Sign-in and sign-out</u></h5>
+<p>This tracks the time that the student signs in and also requires him/her to sign-out.</p>
+<p>Possible uses would be departments such as:</p>
+<ul>
+<li>the library
+<li>student success
+<li>detention room
+<li> etc.
+</ul>
+
+<p> This could be used for
+<ol type="i">
+<li> allowing teachers to see when a student actually arrives and leaves the library / Resource /
+Student Success / Guidance
+<li> verifying that the student has a spare when they check into the library without a slip.
+<li> tracking usage for urban-funding grants
+</ol>
+<p>These kiosks would normally be set to <b>public</b> so that all teachers can see who is there,
+when they arrived, when they left,
+and can search for a particular student.</p>
+<p>If a student forgets to signout, there is an autosignout option that will sign the student out
+at the specified time (e.g. end of period)
+</p>
+</div><!-- end callout -->
+
+<div class="callout callout-success">
+<h5 class="text-dark">2. <u>Present Only</u></h5>
+<p> This is used when you just need a record of who (students only) was present at a meeting and
+signing out doesn't make sense.<br>
+Examples</p>
+<ul>
+<li>Gamer's club (or any club) : used for applying to Student Council for funding, listing students who can get Letters.
+<li>Raider Robotics : taking attendance at the beginning of each general meeting takes an inordinate amount
+of time. This would speed it up significantly.
+<li>BILP : recording attendance in the morning (and at lunch?), formatted in a way such that entry
+to Web Attendance is simple.
+<li>Indoor sports teams : attendance for each practice. <br>
+<i>This probably won't work for outdoor teams since this program is only available via the inhouse
+school network. Wifi wouldn't reach to the field.</i></li>
+</ul>
+
+These kiosks would normally be set to <b>private</b> so that only the teachers running the club/team
+can see the participants and attendance.
+</div><!-- end callout -->
+<div class="callout callout-success">
+
+<h5 class="text-dark">3. <u>Event based for classes</u></h5>
+<p> This mode allows you to specify a class list of students and also an event with a check in time. 
+It shows which students are present/late/absent.<br>
+This was created especially for the Dance Programme, where they have rehearsals and shows and need to track which of the 130 students are missing before the show starts. (Teachers also wanted to know who was late, to deduct marks from those students as well.) </p>
+<p>Details</p>
+
+These kiosks should be set to <b>private</b> so that only the teachers running the event can see who was absent/late.
+</div><!-- end callout -->
+
+</div><!-- end card body -->
+</div><!-- end card -->
+
---
+++ resources/views/child/helpTab2.blade.php
@@ -0,0 +1,48 @@
+<div class="card card-warning">
+<div class="card-header">
+<h4>Database administrators</h4>
+</div>
+<div class="card-body">
+<p><b>The two administrators are Michael Harwood and Larry Farquahrson.</b></p>
+<p>The administrators are the only ones who can:<br>
+<ol>
+<li>add new users and delete users
+<li>add a new kiosk, set the initial kiosk admin, and delete kiosks
+</ol>
+</div>
+</div>
+
+<div class="card card-warning">
+<div class="card-header">
+<h4>Kiosk administrators</h4>
+</div>
+<div class="card-body">
+<p>Each kiosk has one or more kiosk admins.<br> The kiosk admins add users to the kiosk.
+Kiosk admins can also change most of the settings of the kiosk (e.g. making the logs
+		private/public).
+</p>
+</div>
+</div>
+<div class="card card-dark">
+<div class="card-header">
+<h4>Kiosk users</h4>
+</div>
+<div class="card-body">
+<p>Users can start the start the Terminals of their own kiosks.
+Only kiosk users (and kiosk admins) can view the logs of a kiosk if the kiosk is set to private.
+</p>
+<p class="border border-info">Note that by "users" we mean the teachers/staff who use this program to log student attendance. 
+We don't mean the actual students; they are not "users".</p>
+</div>
+</div>
+
+<div class="card card-dark">
+<div class="card-header">
+<h4>General users</h4>
+</div>
+<div class="card-body">
+<p>Any user can view the settings of any kiosk and see who the users/admins of that kiosk are<br>
+Any user can view public logs and can search for students.</p>
+<p>There is a generic teacher login which acts like a user who is not assigned to any kiosks.</p>
+</div>
+</div>
